a1ecdd7040f986764cf5f6c6cf39a81b5e171c40
imtiyaz18 <imtiyazuddin.ug21@nsut.ac.in>
2024-10-11 20:46:03 +0530

    Exploring GET, POST, DELETE Methods

--- src/app.js
+++ src/app.js
@@ -1,13 +1,36 @@
 const express = require('express')
 
 const app = express();
-app.use("/hello", (req, res)=>{
-    res.send("hello hello hello")
+
+// ORDER MATTERS otherwise only this response would be printed in each of the HTTP methods
+// app.use("/user", (req, res)=>{
+//     res.send("hello from the server")
+// })
+
+// this will handle only GET call to /user
+app.get("/user", (req, res) => {
+    res.send("This is GET Method")
 })
 
-app.use("/test", (req, res)=>{
+//this will call only POST method
+app.post("/user", (req, res) => {
+    // saving data to DB
+    res.send("Data sent to DATABASE successfully");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully");
+})
+
+// this will match all the HTTP method API calls to /user
+app.use("/user", (req, res)=>{
     res.send("hello from the server")
 })
+
+// app.use("/", (req, res)=>{
+//     res.send("HELLO WORLD!")
+// })
+
 app.listen(7777, () => {
     console.log("Server is successfully listening on 7777...");
 })
