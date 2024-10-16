f1fdf79f9dfdfdbb2c5e491d6f22a182bc932013
imtiyaz18 <imtiyazuddin.ug21@nsut.ac.in>
2024-10-17 01:33:05 +0530

    NEVER TRUST req.body

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -15,10 +16,20 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         unique: true,
         trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid emailID: " + value)
+            }
+        },
     },
     password: {
         type: String,
         required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Very Weak Password: " + value)
+            }
+        },
     },
     age: {
         type: Number,
@@ -35,6 +46,11 @@ const userSchema = new mongoose.Schema({
     photoUrl: {
         type: String,
         default: "https://www.easyfix.in/public/images/dummy-user.jpg",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Enter a Valid Photo URL: " + value)
+            }
+        },
     },
     about: {
         type: String,
