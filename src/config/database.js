7704ce9c7bb87c8b20829e9a666e82544acc0287
imtiyaz18 <146662405+imtiyaz18@users.noreply.github.com>
2024-11-29 11:30:30 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,10 @@
 const mongoose = require("mongoose");
 
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(
-            "mongodb+srv://imtiyazuddin959:wh8HdBLI7pcq90SQ@backendproject.yla1b.mongodb.net/devLink",
-            { useNewUrlParser: true, useUnifiedTopology: true }
-        );
-        console.log("Database connected successfully");
-    } catch (error) {
-        console.error("Database connection failed:", error.message);
-        throw error; // Propagate the error to indicate failure
-    }
+    await mongoose.connect(
+        "mongodb+srv://imtiyazuddin959:wh8HdBLI7pcq90SQ@backendproject.yla1b.mongodb.net/devLink"
+    );
 };
 
 module.exports = connectDB;
